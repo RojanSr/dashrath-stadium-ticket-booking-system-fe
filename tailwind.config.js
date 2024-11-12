835ef6a2f560be260b6a905b95389eef8feb3e09
RojanSr <rairojan1116@gmail.com>
2024-11-12 18:18:12 +0545

    feat: configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      primary_dark: "#7161ef",
+      primary_light: "#8577f2",
+      secondary: "#e9e8ed",
+    },
+  },
+  plugins: [],
+};
